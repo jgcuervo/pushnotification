d25c462485977b841280aefcc9c7a59192f0e139
jgcuervo <jgcuervo@hotmail.com>
2019-10-10 16:10:18 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('notificationclose', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+
+  console.log('Closed notification: ' + primaryKey);
+});
